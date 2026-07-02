cb36a942c81b0e25b201501c5c214b3d01207847
Mohamed Mahmoud Mohamed <143327189+mohamedmahmoud345@users.noreply.github.com>
2026-07-02 22:18:05 +0300

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public bool IsValid(string s) {
+        var stack = new Stack<char>();
+        var dic = new Dictionary<char, char>() {
+            {'}', '{'},
+            {')', '('},
+            {']', '['}
+        };
+
+        foreach(var ch in s){
+            if(dic.ContainsKey(ch))
+            {
+                if(stack.Count > 0 && stack.Peek() == dic[ch])
+                    stack.Pop();
+                else {
+                    return false;
+                }   
+            }else 
+                stack.Push(ch);
+        }
+
+        return stack.Count == 0;
+    }
+}
